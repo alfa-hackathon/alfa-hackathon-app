ebd561e0e656c50e25160acedfa28745cbbbdc4a
undndnwnkk <bykov.lev74@gmail.com>
2025-11-29 03:15:28 +0300

    init commit

---
+++ backend/services/client-model-service/src/main/java/com/alfahackathon/clientmodelservice/ClientModelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alfahackathon.clientmodelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientModelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientModelServiceApplication.class, args);
+    }
+
+}
---
+++ backend/services/client-model-service/src/test/java/com/alfahackathon/clientmodelservice/ClientModelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfahackathon.clientmodelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientModelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
